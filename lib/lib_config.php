f09f8d2869d9d6c5542d232a8e8e0666051923f9
derek2016 <550891945@qq.com>
2016-10-26 14:12:22 +0800

    vivan

---
+++ lib/lib_config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Config
+ *
+ * @author DEREK
+ *         2016-10-21
+ */
+
+// 系统参数
+// database host
+$db_host = "localhost:3306";
+$db_name = "vivian_db";
+$db_user = "root";
+$db_pass = "";
+
+// 系统常量
+$smarty->assign("web_title", "测试");
+$smarty->assign("page_name", "");

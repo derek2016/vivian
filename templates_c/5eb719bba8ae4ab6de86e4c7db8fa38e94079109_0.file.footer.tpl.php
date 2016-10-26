5f28a8ea6271e4586a8e5bdc91f95419bd8b79d4
derek2016 <550891945@qq.com>
2016-10-26 14:07:46 +0800

    lib

---
+++ templates_c/5eb719bba8ae4ab6de86e4c7db8fa38e94079109_0.file.footer.tpl.php
@@ -0,0 +1,114 @@
+<?php
+/* Smarty version 3.1.30, created on 2016-10-26 07:51:40
+  from "D:\ECProject\vivian\vivian\templates\footer.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5810446c6ef8a0_42338923',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '5eb719bba8ae4ab6de86e4c7db8fa38e94079109' => 
+    array (
+      0 => 'D:\\ECProject\\vivian\\vivian\\templates\\footer.tpl',
+      1 => 1477461037,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5810446c6ef8a0_42338923 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<footer id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				Copyright &copy; 2016. &nbsp; | &nbsp; VIVIAN&DEREK All rights reserved.
+				<br>
+				<a target="_blank" href="http://www.miitbeian.gov.cn/">冀ICP备16002734号</a>
+			</div>
+			<div class="col-sm-6">
+				<ul class="social-icons">
+					<li>
+						<a href="#">
+							<i class="fa fa-facebook"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-twitter"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-google-plus"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-linkedin"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-youtube"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-github"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+<!--/#footer-->
+
+<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['web_js']->value;?>
+/jquery.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['web_js']->value;?>
+/bootstrap.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['web_js']->value;?>
+/owl.carousel.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['web_js']->value;?>
+/mousescroll.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['web_js']->value;?>
+/smoothscroll.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['web_js']->value;?>
+/jquery.prettyPhoto.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['web_js']->value;?>
+/jquery.isotope.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['web_js']->value;?>
+/jquery.inview.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['web_js']->value;?>
+/wow.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['web_js']->value;?>
+/custom-scripts.js"><?php echo '</script'; ?>
+>
+</body>
+</html><?php }
+}
